929a36930356a0fb51aa948fd091c5bdd1dbc101
parakkramadasanayaka.2 <parakkramadasanayaka.2@gmail.com>
2024-10-27 18:17:59 +0000

    flutter setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kmwd/screens/Home.dart';
 import 'package:kmwd/screens/authentcation/LoginScreen.dart';
 import 'package:kmwd/screens/authentcation/Register.dart';
 
-void main() {
+void main() async{
+WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb){
+  await  Firebase.initializeApp(options: FirebaseOptions( apiKey: "AIzaSyBpYUp0fAU7tvxz4BfwWMvDnLz86Rfcoww",
+  authDomain: "funitureapp-37893.firebaseapp.com",
+  projectId: "funitureapp-37893",
+  storageBucket: "funitureapp-37893.appspot.com",
+  messagingSenderId: "981496557296",
+  appId: "1:981496557296:web:e553cc872403bb249b1c35",
+  measurementId: "G-SB0GSD6DR9"));
+  }
+ else{
+   await Firebase.initializeApp();
+ }
+
   runApp(const MyApp());
 }
 
@@ -24,13 +41,13 @@ class _MyAppState extends State<MyApp> {
       home:const SafeArea(
         child: Padding(
           padding: EdgeInsets.all(4),
-          child:const Home()
+          child:Home()
         ),
       ),
       routes: <String,WidgetBuilder>{
-        '/register': (context) => Register(),
-        '/login': (context)=>Loginscreen(),
-        '/home':(context)=>Home()
+        '/register': (context) => const Register(),
+        '/login': (context)=>const Loginscreen(),
+        '/home':(context)=>const Home()
       },
       //hghg
     );
